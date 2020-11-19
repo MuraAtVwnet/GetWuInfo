fd6c3ef5aa1208082e3712aa09ed846f86507340
Syuichi Murashima <mura-git@vwnet.jp>
2020-11-19 12:52:20 +0900

    テストコードアップ

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,8 @@
-﻿class UpdateClass {
+﻿
+
+class UpdateClass {
 	[string] $CVE
-	[string] $Date
+	# [string] $Date
 	[string] $KB
 	# [string] $Type
 	[string] $SubType
@@ -8,15 +10,113 @@
 	[string] $URI
 }
 
-
-if( (Get-Module MSRCSecurityUpdates) -eq $null ){
-	Install-Module MSRCSecurityUpdates -Force
+$MonthTable = @{
+	1 = "Jan"
+	2 = "Feb"
+	3 = "Mar"
+	4 = "Apr"
+	5 = "May"
+	6 = "Jun"
+	7 = "Jul"
+	8 = "Aug"
+	9 = "Sep"
+	10 = "Oct"
+	11 = "Nov"
+	12 = "Dec"
 }
+
+$ClientOsVertionTable = @{
+	"10240" = "Windows 10 for"
+	"1607" = "Windows 10 Version 1607"
+	"1803" = "Windows 10 Version 1803"
+	"1809" = "Windows 10 Version 1809"
+	"1903" = "Windows 10 Version 1903"
+	"1909" = "Windows 10 Version 1909"
+	"2004" = "Windows 10 Version 2004"
+	"20H2" = "Windows 10 Version 20H2"
+}
+
+<#
+$ServerOsVertionTable = @{
+	"xxxx" = "Windows Server 2012"
+	"xxxx" = "Windows Server 2012 R2"
+	"xxxx" = "Windows Server 2016"
+	"xxxx" = "Windows Server 2019"
+	"xxxx" = "Windows Server, version 1903"
+	"xxxx" = "Windows Server, version 2004"
+	"xxxx" = "Windows Server, version 20H2"
+}
+#>
+
+###############################################
+# Windows Update 日を取得する(日本)
+###############################################
+function GetWindowsUpdateDay([datetime]$TergetDate){
+
+	# 1日の曜日と US Windows Update 日のオフセット ハッシュテーブル
+	$DayOfWeek2WUOffset = @{
+		[System.DayOfWeek]"Wednesday"	= 13	# 水曜日
+		[System.DayOfWeek]"Thursday"	= 12	# 木曜日
+		[System.DayOfWeek]"Friday"		= 11	# 金曜日
+		[System.DayOfWeek]"Saturday"	= 10	# 土曜日
+		[System.DayOfWeek]"Sunday"		= 9 	# 日曜日
+		[System.DayOfWeek]"Monday"		= 8 	# 月曜日
+		[System.DayOfWeek]"Tuesday" 	= 7 	# 火曜日
+	}
+
+	# 年月が指定されていない(default)
+	if( $TergetDate -eq $null ){
+		# 今の日時
+		$TergetDate = Get-Date
+	}
+
+	# 1日
+	$1stDay = [datetime]$TergetDate.ToString("yyyy/MM/1")
+
+	# US Windows Update 日のオフセット
+	$Offset = $DayOfWeek2WUOffset[$1stDay.DayOfWeek]
+
+	if( $Offset -ne $null ){
+		# US Windows Update 日
+		$WUDayUS = $1stDay.AddDays($Offset)
+
+		# 日本の Windows Update 日(US Windows Update の翌日)
+		$WUDay = $WUDayUS.AddDays(1)
+	}
+	else{
+		$WUDay = $null
+	}
+
+	return ($WUDay).ToString("yyyy/MM/dd")
+}
+
+###############################################
+# Main
+###############################################
+# 今月のWu日
+[datetime]$WuDay = GetWindowsUpdateDay
+
+# 今日
+$Today = Get-Date
+
+# WU 日前だったら前月
+if( $Today -lt $WuDay ){
+	$TergetDay = $Today.AddMonths(-1)
+}
+else{
+	$TergetDay = $Today
+}
+
+$TergetMonth = [string]$TergetDay.Year + "-" + $MonthTable[$TergetDay.Month]
+
+#if( (Get-Module MSRCSecurityUpdates) -eq $null ){
+#	Install-Module MSRCSecurityUpdates -Force
+#}
 
 Import-Module MSRCSecurityUpdates
 
 Set-MSRCApiKey -Verbose -ApiKey "7ac1e01ac58840c190f9e519ccf7cb47"
-$cvrfDoc = Get-MsrcCvrfDocument -ID 2020-Nov
+$cvrfDoc = Get-MsrcCvrfDocument -ID $TergetMonth
 
 # プロダクト名
 $ProductTree = $cvrfDoc.ProductTree
@@ -31,7 +131,9 @@ $Updates = @()
 foreach( $Vulnerability in $Vulnerabilitys){
 
 	$CVE = $Vulnerability.CVE
-	$Date = $Vulnerability.RevisionHistory.Date
+
+	# [array]$Dates = $Vulnerability.RevisionHistory.Date
+    # $Date = ($Dates | Sort-Object -Descending)[0]
 
 	$Remediations = $Vulnerability.Remediations | ? Type -eq 2
 
@@ -48,7 +150,7 @@ foreach( $Vulnerability in $Vulnerabilitys){
 					$Update = New-Object UpdateClass
 
 					$Update.CVE = $CVE
-					$Update.Date = $Date.ToString()
+					# $Update.Date = $Date.ToString()
 					$Update.KB = $KB
 					# $Update.Type = $Type
 					$Update.SubType = $SubType
@@ -62,8 +164,58 @@ foreach( $Vulnerability in $Vulnerabilitys){
 	}
 }
 
-$Updates | Sort-Object -Property KB, ProductName -Unique | Export-Csv C:\Test\WUs.csv -Encoding OEM
+# $Updates | Export-Csv C:\Test\WUs.csv -Encoding OEM
 
+# $Updates | Sort-Object -Property KB, ProductName -Unique | Export-Csv C:\Test\WUs.csv -Encoding OEM
+
+
+# OS のエディション
+$Win32_OperatingSystem = Get-WmiObject Win32_OperatingSystem
+$OS = $Win32_OperatingSystem.Caption
+
+# Windows 10
+if( $OS  -match "Windows 10" ){
+	# OS バージョン
+	$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+	$RegKey = "ReleaseId"
+	$OSVertion = (Get-ItemProperty $RegPath -name $RegKey -ErrorAction SilentlyContinue).$RegKey
+
+	if( $OSVertion -eq $null ){
+		$OSVertion = "10240"
+	}
+}
+else{
+	echo "対応していない Windows OS です"
+	echo "必要に応じて拡張実装してください"
+	exit
+}
+
+$SelectString = $ClientOsVertionTable[$OSVertion]
+
+[array]$WuKBs = $Updates | ? ProductName -Match $SelectString | Sort-Object -Property KB -Unique
+
+echo $WuKBs
+
+[array]$HotFixs = Get-Hotfix
+
+$HitFlag = $false
+
+foreach ( $WuKB in $WuKBs ){
+    $KbName = "KB" + $WuKB.KB
+    if( $HotFixs.HotFixID -contains $KbName ){
+        $HitFlag = $true
+    }
+    else{
+        $HitFlag = $false
+    }
+}
+
+if( $HitFlag ){
+    echo "最新の WU が当たっています"
+}
+else{
+    echo "最新の WU が当たっていません"
+}
 
 <#
 
@@ -76,4 +228,3 @@ $Updates | ? ProductName -Match "Windows 10 Version 1909" | Sort-Object -Propert
 
 #>
 
-# $Updates | Export-Csv C:\Test\WUs.csv -Encoding OEM
