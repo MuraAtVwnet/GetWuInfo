14364ba04b800bd8d3f159d16992a8ff6361541f
Syuichi Murashima <mura-git@vwnet.jp>
2020-11-25 09:48:56 +0900

    公開用に調整

--- Test.ps1
+++ Test.ps1
@@ -1,32 +1,5 @@
 ﻿
-
-# セキュリティ更新を格納するオブジェクト
-class UpdateClass {
-	[string] $CVE			# CVE 番号
-	[string] $ReleaseDate	# リリース日
-	[string] $KB			# KB 番号
-	[string] $SubType		# 更新の種類
-	[string] $ProductID		# プロダクト ID
-	[string] $ProductName	# プロダクト名
-	[string] $URI			# KB の URI
-}
-
-$MonthTable = @{
-	1 = "Jan"
-	2 = "Feb"
-	3 = "Mar"
-	4 = "Apr"
-	5 = "May"
-	6 = "Jun"
-	7 = "Jul"
-	8 = "Aug"
-	9 = "Sep"
-	10 = "Oct"
-	11 = "Nov"
-	12 = "Dec"
-}
-
-$ClientOsVertionTable = @{
+$Windows10VertionTable = @{
 	"10240" = "Windows 10 for"
 	"1607" = "Windows 10 Version 1607"
 	"1803" = "Windows 10 Version 1803"
@@ -37,17 +10,15 @@ $ClientOsVertionTable = @{
 	"2009" = "Windows 10 Version 20H2"
 }
 
-<#
-$ServerOsVertionTable = @{
-	"xxxx" = "Windows Server 2012"
-	"xxxx" = "Windows Server 2012 R2"
-	"xxxx" = "Windows Server 2016"
-	"xxxx" = "Windows Server 2019"
-	"xxxx" = "Windows Server, version 1903"
-	"xxxx" = "Windows Server, version 2004"
-	"xxxx" = "Windows Server, version 20H2"
+$WindowsServerVertionTable = @{
+#	"xxxx" = "Windows Server 2012"
+#	"xxxx" = "Windows Server 2012 R2"
+#	"xxxx" = "Windows Server 2016"
+	"1809" = "Windows Server 2019"
+#	"xxxx" = "Windows Server, version 1903"
+#	"xxxx" = "Windows Server, version 2004"
+#	"xxxx" = "Windows Server, version 20H2"
 }
-#>
 
 ###############################################
 # Windows Update 日を取得する(日本)
@@ -91,13 +62,132 @@ function GetWindowsUpdateDay([datetime]$TergetDate){
 	return ($WUDay).ToString("yyyy/MM/dd")
 }
 
+
+###############################################
+# 指定月に発行されたセキュリティ更新取得
+# https://msrc.microsoft.com/update-guide/
+###############################################
+function GetSecurityUpdateKBs([datetime]$TergetDay){
+	# API Key
+	# Microsoft Security Update API(@outlook.com、@live.com MSA が必要)
+	# https://portal.msrc.microsoft.com/ja-jp/developer
+	$APIKey = "7ac1e01ac58840c190f9e519ccf7cb47"
+
+	# セキュリティ更新を格納するオブジェクト
+	class UpdateClass {
+		[string] $CVE			# CVE 番号
+		[string] $ReleaseDate	# リリース日
+		[string] $KB			# KB 番号
+		[string] $SubType		# 更新の種類
+		[string] $ProductID		# プロダクト ID
+		[string] $ProductName	# プロダクト名
+		[string] $URI			# KB の URI
+	}
+
+	# 月の略称
+	$MonthTable = @{
+		1 = "Jan"
+		2 = "Feb"
+		3 = "Mar"
+		4 = "Apr"
+		5 = "May"
+		6 = "Jun"
+		7 = "Jul"
+		8 = "Aug"
+		9 = "Sep"
+		10 = "Oct"
+		11 = "Nov"
+		12 = "Dec"
+	}
+
+	$TergetMonth = [string]$TergetDay.Year + "-" + $MonthTable[$TergetDay.Month]
+
+	try{
+		Import-Module MSRCSecurityUpdates -ErrorAction Stop
+	}
+	catch{
+		echo "必要モジュールがインストールされていません"
+		echo "以下手順でモジュールをインストールしてください(要管理権限)"
+		echo "Install-Module MSRCSecurityUpdates -Force"
+		exit
+	}
+
+	Set-MSRCApiKey -ApiKey $APIKey
+	$cvrfDoc = Get-MsrcCvrfDocument -ID $TergetMonth
+
+	# プロダクト名
+	$FullProductName = $cvrfDoc.ProductTree.FullProductName
+
+
+	# プロダクト別のセキュリティ更新
+	$Updates = @()
+
+	# セキュリティ更新
+	[array]$Vulnerabilitys = $cvrfDoc.Vulnerability
+
+	# CVE ごとの処理
+	foreach( $Vulnerability in $Vulnerabilitys){
+
+		$CVE = $Vulnerability.CVE
+
+		# リリース日
+		[datetime]$ReleaseDate = ($Vulnerability.RevisionHistory | Sort-Object -Property Date)[0].Date
+
+		# セキュリティ更新の抽出
+		[array]$Remediations = $Vulnerability.Remediations | ? Type -eq 2
+
+		# セキュリティ更新ごとの処理
+		foreach( $Remediation in $Remediations ){
+
+			# KB 番号が数値のデータの未処理
+			if($Remediation.Description.Value -match "[0-9]+"){
+
+				# KB 番号
+				$KB = $Remediation.Description.Value
+
+				# 更新の種類
+				$SubType = $Remediation.SubType
+
+				# プロダクトの展開
+				[array]$ProductIDs = $Remediation.ProductID
+
+				# プロダクト後の処理
+				foreach( $ProductID in $ProductIDs ){
+					# 各値のセット
+					$Update = New-Object UpdateClass
+
+					$Update.CVE = $CVE
+					$Update.ReleaseDate = $ReleaseDate.ToString()
+					$Update.KB = $KB
+					$Update.SubType = $SubType
+					$Update.ProductID = $ProductID
+					$Update.ProductName = ($FullProductName | ? ProductID -eq $ProductID).Value
+					$Update.URI = "https://support.microsoft.com/ja-jp/help/" + $KB
+
+					$Updates += $Update
+				}
+			}
+		}
+	}
+
+	return $Updates
+}
+
+
+
 ###############################################
 # Main
 ###############################################
 
-if( ($PSVersionTable.PSVersion.Major -ne 5) -or ($PSVersionTable.PSVersion.Minor -ne 1)){
-	echo "このスクリプト実行には Windows PowerShell 5.1 が必要です"
-	echo "https://docs.microsoft.com/ja-jp/powershell/scripting/windows-powershell/install/windows-powershell-system-requirements"
+if( ($PSVersionTable.PSVersion.Major -ge 5) ){
+	if(($PSVersionTable.PSVersion.Major -eq 5) -and ($PSVersionTable.PSVersion.Minor -ne 1)){
+		echo "このスクリプト実行には Windows PowerShell 5.1 が必要です"
+		echo "https://docs.microsoft.com/ja-jp/powershell/scripting/windows-powershell/install/windows-powershell-system-requirements"
+		exit
+	}
+}
+else{
+	echo "このスクリプト実行には Windows PowerShell 5.1 または PowerShell Core が必要です"
 	exit
 }
 
@@ -117,80 +207,10 @@ else{
 	$TergetDay = $Today
 }
 
-$TergetMonth = [string]$TergetDay.Year + "-" + $MonthTable[$TergetDay.Month]
+# KB 取得
+[array]$Updates = GetSecurityUpdateKBs $TergetDay
 
-try{
-	Import-Module MSRCSecurityUpdates -ErrorAction Stop
-}
-catch{
-	Install-Module MSRCSecurityUpdates -Force
-	Import-Module MSRCSecurityUpdates
-}
-
-
-Set-MSRCApiKey -ApiKey "7ac1e01ac58840c190f9e519ccf7cb47"
-$cvrfDoc = Get-MsrcCvrfDocument -ID $TergetMonth
-
-# プロダクト名
-$FullProductName = $cvrfDoc.ProductTree.FullProductName
-
-
-# プロダクト別のセキュリティ更新
-$Updates = @()
-
-# セキュリティ更新
-[array]$Vulnerabilitys = $cvrfDoc.Vulnerability
-
-# CVE ごとの処理
-foreach( $Vulnerability in $Vulnerabilitys){
-
-	$CVE = $Vulnerability.CVE
-
-	# リリース日
-	[datetime]$ReleaseDate = ($Vulnerability.RevisionHistory | Sort-Object -Property Date)[0].Date
-
-	# セキュリティ更新の抽出
-	[array]$Remediations = $Vulnerability.Remediations | ? Type -eq 2
-
-	# セキュリティ更新ごとの処理
-	foreach( $Remediation in $Remediations ){
-
-		# KB 番号が数値のデータの未処理
-		if($Remediation.Description.Value -match "[0-9]+"){
-
-			# KB 番号
-			$KB = $Remediation.Description.Value
-
-			# 更新の種類
-			$SubType = $Remediation.SubType
-
-			# プロダクトの展開
-			[array]$ProductIDs = $Remediation.ProductID
-
-			# プロダクト後の処理
-			foreach( $ProductID in $ProductIDs ){
-
-				# プロダクト ID からプロダクト名を取得
-				$ProductName = ($FullProductName | ? ProductID -eq $ProductID).Value
-				if( $ProductName -match "^Windows" ){
-
-					# 各値のセット
-					$Update = New-Object UpdateClass
-					$Update.CVE = $CVE
-					$Update.ReleaseDate = $ReleaseDate.ToString()
-					$Update.KB = $KB
-					$Update.SubType = $SubType
-					$Update.ProductID = $ProductID
-					$Update.ProductName = $ProductName
-					$Update.URI = "https://support.microsoft.com/ja-jp/help/" + $KB
-
-					$Updates += $Update
-				}
-			}
-		}
-	}
-}
-
+$Updates | Sort-Object -Property ProductName, KB -Unique | Export-Csv ~\Documents\KBs.csv -Encoding OEM
 
 # $Updates | Export-Csv C:\Test\WUs.csv -Encoding OEM
 
@@ -202,24 +222,37 @@ $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
 $RegKey = "ProductName"
 $OS = (Get-ItemProperty $RegPath -name $RegKey -ErrorAction SilentlyContinue).$RegKey
 
+# OS バージョン
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+$RegKey = "ReleaseId"
+$OSVertion = (Get-ItemProperty $RegPath -name $RegKey -ErrorAction SilentlyContinue).$RegKey
+
+
 # Windows 10
 if( $OS  -match "Windows 10" ){
-	# OS バージョン
-	$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-	$RegKey = "ReleaseId"
-	$OSVertion = (Get-ItemProperty $RegPath -name $RegKey -ErrorAction SilentlyContinue).$RegKey
 
 	if( $OSVertion -eq $null ){
 		$OSVertion = "10240"
 	}
+
+	$SelectString = $Windows10VertionTable[$OSVertion]
+}
+# Windows Server 実装 例(例レベルなので、Windows Server で使うのであればちゃんと実装する必要あり)
+elseif( $OS  -match "Windows Server" ){
+	if( $WindowsServerVertionTable.ContainsKey($OSVertion) ){
+		$SelectString = $WindowsServerVertionTable[$OSVertion]
+	}
+	else{
+		echo "対応していない Windows Server OS バージョンです"
+		echo "必要に応じて拡張実装してください"
+		exit
+	}
 }
 else{
-	echo "対応していない Windows OS です"
+	echo "対応していない Windows OS バージョンです"
 	echo "必要に応じて拡張実装してください"
 	exit
 }
-
-$SelectString = $ClientOsVertionTable[$OSVertion]
 
 [array]$WuKBs = $Updates | ? ProductName -Match $SelectString | Sort-Object -Property KB -Unique
 
@@ -245,15 +278,4 @@ if( $HitFlag ){
 else{
 	echo "最新の WU が当たっていません"
 }
-
-<#
-
-WU 日過ぎていたら今月 else 前月
-
-マッチングさせる ProductName をハッシュテーブルで定義し、OS 情報から ProductName を特定する
-対象の ProductName のみを抽出し、KB でユニークにすると最新の WU が判明する
-
-$Updates | ? ProductName -Match "Windows 10 Version 1909" | Sort-Object -Property KB -Unique
-
-#>
 
